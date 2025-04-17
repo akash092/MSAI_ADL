4e0a6efd20fb91483ccafb696d4c8202a52561b5
akash092 <akash.taken@gmail.com>
2025-04-16 21:27:41 -0700

    HW 3 part 4 first attempt

--- homework3_v3/homework/datagen.py
+++ homework3_v3/homework/datagen.py
@@ -1,7 +1,45 @@
-def generate_dataset(output_json: str, oversample: int = 10, temperature: float = 0.6):
-    raise NotImplementedError()
+from .data import Dataset
+from .cot import CoTModel
+import json
+
+def is_answer_valid(answer: float, correct_answer: float, relative_tolerance: float = 0.05):
+    abs_diff = abs(round(answer, 3) - round(correct_answer, 3))
+    valid = abs_diff < relative_tolerance * abs(round(correct_answer, 3))
+    return valid, abs_diff
 
 
+def generate_dataset(output_json: str = 'data/rft.json', oversample: int = 10, temperature: float = 0.6):
+    #raise NotImplementedError()
+    dataset = Dataset("train")
+    idx = range(len(dataset))
+
+    llm = CoTModel()
+    data = []
+    count = 0
+    for idx in range(len(dataset)):
+        question = dataset[idx][0]
+        answer = dataset[idx][1]
+        prompt = llm.format_prompt(question)
+        generations = llm.batched_generate([prompt], oversample, temperature)
+        print(generations)
+        parsed_answers = [llm.parse_answer(g) for g in generations[0]]
+        min_diff = -1
+        min_diff_idx = -1
+        for i in range(len(parsed_answers)):
+            valid, diff = is_answer_valid(parsed_answers[i], answer)
+            if valid and (diff < min_diff or min_diff == -1):
+                min_diff = diff
+                min_diff_idx = i
+        
+        if min_diff_idx != -1:
+            # write this data to file
+            data.append([question, parsed_answers[min_diff_idx], generations[min_diff_idx]])
+            count += 1
+            print("found an entry, count:", count)
+
+    with open(output_json, 'w') as file:
+        json.dump(data, file, indent=4)
+        
 if __name__ == "__main__":
     from fire import Fire
 
--- homework3_v3/homework/rft.py
+++ homework3_v3/homework/rft.py
@@ -1,6 +1,9 @@
 from .base_llm import BaseLLM
-from .sft import test_model
-
+from .sft import test_model, TokenizedDataset
+from .data import Dataset
+from transformers import Trainer, TrainingArguments
+from peft import PeftModel, LoraConfig, get_peft_model
+from pathlib import Path
 
 def load() -> BaseLLM:
     from pathlib import Path
@@ -17,12 +20,57 @@ def load() -> BaseLLM:
     return llm
 
 
+def format_example(prompt: str, answer: str, reasoning: str) -> dict[str, str]:
+    return {"question": prompt, "answer": reasoning}
+
+
 def train_model(
     output_dir: str,
     **kwargs,
 ):
     # Reuse much of the SFT code here
-    raise NotImplementedError()
+    #raise NotImplementedError()
+
+    llm = BaseLLM()
+    peft_config = LoraConfig(
+        target_modules="all-linear",
+        bias="none",
+        task_type="CAUSAL_LM", 
+        inference_mode=False, 
+        r=8, 
+        lora_alpha=32, 
+        lora_dropout=0.1
+    )
+
+    model = get_peft_model(llm.model, peft_config)
+    model.enable_input_require_grads()
+    model.print_trainable_parameters()
+
+    training_args = TrainingArguments(
+        gradient_checkpointing=True,
+        learning_rate=1e-3,
+        output_dir=output_dir,
+        logging_dir=output_dir,
+        report_to="tensorboard",
+        per_device_train_batch_size=32,
+        num_train_epochs=5,
+        # weight_decay=0.01,
+        # eval_strategy="epoch",
+        # save_strategy="epoch",
+        # load_best_model_at_end=True,
+    )
+    
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=TokenizedDataset(llm.tokenizer, Dataset("rft"), format_example)
+    )
+
+    trainer.train()
+    model_name = "rft_model"
+    model_path = Path(__file__).parent / model_name
+    model.save_pretrained(model_path)
+
 
 
 if __name__ == "__main__":
